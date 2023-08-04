c5d58023c8c8f2fd400ff30742988020ca790f4d
For Fork Sake <sakeforfork@gmail.com>
2023-08-04 16:40:20 +0100

    Ofox beta release
    Decryption excluded for the time being.
    
    Fixed a build issue where the recovery image was always 12 bytes oversized?
    assumed it was size of additional binaries so initiallly removed unnecessary such as python, aapt, build currently still excludes them.

---
+++ vendorsetup.sh
@@ -0,0 +1,113 @@
+ #
+#	This file is part of the OrangeFox Recovery Project
+# 	Copyright (C) 2022 The OrangeFox Recovery Project
+#
+#	OrangeFox is free software: you can redistribute it and/or modify
+#	it under the terms of the GNU General Public License as published by
+#	the Free Software Foundation, either version 3 of the License, or
+#	any later version.
+#
+#	OrangeFox is distributed in the hope that it will be useful,
+#	but WITHOUT ANY WARRANTY; without even the implied warranty of
+#	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#	GNU General Public License for more details.
+#
+# 	This software is released under GPL version 3 or any later version.
+#	See <http://www.gnu.org/licenses/>.
+#
+# 	Please maintain this if you use this script or any part of it
+#
+FDEVICE="a14m"
+#set -o xtrace
+
+fox_get_target_device() {
+local chkdev=$(echo "$BASH_SOURCE" | grep -w $FDEVICE)
+   if [ -n "$chkdev" ]; then
+      FOX_BUILD_DEVICE="$FDEVICE"
+   else
+      chkdev=$(set | grep BASH_ARGV | grep -w $FDEVICE)
+      [ -n "$chkdev" ] && FOX_BUILD_DEVICE="$FDEVICE"
+   fi
+}
+
+if [ -z "$1" -a -z "$FOX_BUILD_DEVICE" ]; then
+   fox_get_target_device
+fi
+
+if [ "$1" = "$FDEVICE" -o "$FOX_BUILD_DEVICE" = "$FDEVICE" ]; then
+	export TW_DEFAULT_LANGUAGE="en"
+	export LC_ALL="C"
+	export ALLOW_MISSING_DEPENDENCIES=true
+
+	#reduce ofox size
+	export FOX_REMOVE_AAPT=1
+	export FOX_REMOVE_BUSYBOX_BINARY=1
+
+	#Zip install fix - Non standard Boot Location
+	export FOX_RECOVERY_BOOT_PARTITION="/dev/block/platform/bootdevice/by-name/boot"
+	export FOX_RECOVERY_INSTALL_PARTITION="/dev/block/platform/bootdevice/by-name/recovery"
+	export FOX_RECOVERY_SYSTEM_PARTITION="/dev/block/platform/bootdevice/by-name/system"
+	export FOX_RECOVERY_VENDOR_PARTITION="/dev/block/platform/bootdevice/by-name/vendor"
+
+	# flashlight not working
+	export OF_FLASHLIGHT_ENABLE=1
+	export OF_FL_PATH1="/sys/devices/virtual/camera/flash/rear_torch_flash"
+	export OF_FL_PATH2="/sys/devices/virtual/camera/flash/rear_torch_flash"
+
+	# Explicit Do Not Patch vbmeta with magisk - Fastboot flash args suffice
+	export OF_PATCH_VBMETA_FLAG=0
+	export OF_PATCH_AVB20=1
+	export OF_DISABLE_DM_VERITY_FORCED_ENCRYPTION=1
+
+	# AB and VARs reccomended by ofox devs
+	export OF_USE_TWRP_SAR_DETECT=0
+	export FOX_AB_DEVICE=0
+	export FOX_USE_TWRP_RECOVERY_IMAGE_BUILDER=1
+	export OF_NO_TREBLE_COMPATIBILITY_CHECK=1
+	export OF_NO_MIUI_PATCH_WARNING=1
+	export OF_VANILLA_BUILD=0
+
+	export OF_SUPPORT_PRE_FLASH_SCRIPT=1
+
+	# run a process after formatting data to work-around MTP issues
+	export OF_RUN_POST_FORMAT_PROCESS=1
+
+	# Custom pic for maintainer's about section info
+	export OF_MAINTAINER_AVATAR="/recovery/root/twres/images/Default/maintainer.png"
+
+	#Metadata restore resolves alot of bootloops
+	export OF_QUICK_BACKUP_LIST="/boot;/metadata;"
+
+	# tools
+	export FOX_USE_BASH_SHELL=1
+	export FOX_ASH_IS_BASH=1
+	export FOX_USE_TAR_BINARY=1
+	export FOX_USE_SED_BINARY=1
+	export FOX_USE_XZ_UTILS=1
+	export FOX_REPLACE_TOOLBOX_GETPROP=1
+	export FOX_USE_NANO_EDITOR=1
+	export OF_ENABLE_LPTOOLS=1
+	export FOX_ENABLE_APP_MANAGER=1
+	export FOX_DELETE_AROMAFM=1
+
+	export FOX_VARIANT="ForForkSake"
+	export FOX_BUILD_TYPE="beta"
+	export OF_MAINTAINER="@CardiffIan CFkod@xda"
+
+	# Screen Notch and status bar settings
+	export OF_SCREEN_H=2408
+	export OF_STATUS_H=75
+	export OF_STATUS_INDENT_LEFT=48
+	export OF_STATUS_INDENT_RIGHT=48
+	export OF_CLOCK_POS=1
+	export OF_ALLOW_DISABLE_NAVBAR=0
+	export OF_HIDE_NOTCH=1
+
+	# build vars
+	if [ -n "$FOX_BUILD_LOG_FILE" -a -f "$FOX_BUILD_LOG_FILE" ]; then
+		export | grep "FOX" >> $FOX_BUILD_LOG_FILE
+		export | grep "OF_" >> $FOX_BUILD_LOG_FILE
+		export | grep "TARGET_" >> $FOX_BUILD_LOG_FILE
+		export | grep "TW_" >> $FOX_BUILD_LOG_FILE
+	fi
+fi
